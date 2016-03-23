a667ed2ec77e2dae3da20a5cc04ddb6fc9706cec
nassir <nassirmz@gmail.com>
2016-03-23 16:25:29 -0700

    Return index.html on GET

--- web/request-handler.js
+++ web/request-handler.js
@@ -1,7 +1,24 @@
 var path = require('path');
 var archive = require('../helpers/archive-helpers');
+var fs = require('fs');
+var httpHelper = require('./http-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
-  res.end(archive.paths.list);
+  //console.log('below this line');
+  console.log(__dirname);
+  if (req.method === 'GET') {
+    console.log('it is a get');
+    var file = fs.readFile(__dirname + '/public/index.html', 'utf-8', function(err, data) {
+      //console.log(data);
+      
+      res.writeHead(200, httpHelper.headers);
+      res.end(data);
+
+      // need writeHead
+      //res thing 
+      // status code
+    });
+  }
+  
 };
